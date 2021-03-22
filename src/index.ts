ab88c99649fa83008b3a660ac68ad9f19047d1a5
xlf <you@example.com>
2021-03-22 11:59:56 +0800

    feat:添加类型文件，添加PS class 入口配置判断

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-//
-const VAL: string = 'value';
-console.log('ps ts !!!', VAL);
+// 入口
+
+import { Options } from './types/index';
+class PS {
+  private _options: Options;
+  public context: HTMLElement | null;
+  constructor(options: Options) {
+    this._options = Object.assign(options, {
+      width: '100%',
+      height: '100%'
+    });
+    if (!this._options.query) {
+      throw new Error(' new ps class,must has query filed!!!');
+    }
+    this.context = document.querySelector(this._options.query);
+
+    if (!this.context) {
+      throw new Error(`can't found the ${this._options.query} element!!!`);
+    }
+  }
+}
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export interface Options {
+  query: string; // 选择器 querySelector
+  width?: number | string; //宽
+  height?: number | string; //高
+  plugins?: Array<string>; //插件集合
+}
